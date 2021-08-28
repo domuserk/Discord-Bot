d4ae607c26384d80a9863a5574dfd429fef9451a
domuserk <rgdesenhos321@gmail.com>
2021-08-28 17:15:19 -0300

    add embed link

--- src/bot.js
+++ src/bot.js
@@ -2,9 +2,12 @@ const fs = require('fs')
 const Discord = require('discord.js');
 
 const { prefix, token } = require('../config/config.json');
+
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.cooldowns = new Discord.Collection();
+
+const { MessageEmbed } = require('discord.js');
 
 class NakamotoBot {
     constructor() {
@@ -12,15 +15,26 @@ class NakamotoBot {
     }
 } 
 
-NakamotoBot.prototype.init = async (newAnime) =>  {
+NakamotoBot.prototype.init = async (newAnime, newEpisodeUrl) =>  {
     client.on('ready', async () => {
         const idChannel = '849404264069595176'
         console.log('Bot Already!');
         const channel = await client.channels.fetch(idChannel);
-        if(!newAnime) {
-          return;
+
+        if(newEpisodeUrl) {
+            const embeddLink = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(newAnime)
+            .setURL(newEpisodeUrl)
+            .addFields(
+                { name: `Novo episódio de ${newAnime}`},
+            )
+            .setImage('https://i.imgur.com/AfFp7pu.png')
+            .setTimestamp()
+            .setFooter('By Ryan G. Lopes', 'https://i.imgur.com/rM6VcQW.jpg');
+
+            channel.send(embeddLink);
         }
-        channel.send(newAnime);
     });
     client.login(token);
     await NakamotoBot.prototype.resetBot()
